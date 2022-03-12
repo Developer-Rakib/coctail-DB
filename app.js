416ca310e5347cdbf1ac8681e5aac249f253d967
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-03-12 18:54:24 +0600

    done

--- app.js
+++ app.js
@@ -1,77 +1,87 @@
 
-        // select element 
-        let inputFeild = document.querySelector("#input-feild");
-        let seacrhBtn = document.querySelector("#search-btn");
-        let coctails = document.querySelector("#coctails");
-        let coctail = document.querySelector("#coctail");
-        let load = document.querySelector("#loading");
+// select element 
+let inputFeild = document.querySelector("#input-feild");
+let seacrhBtn = document.querySelector("#search-btn");
+let coctails = document.querySelector("#coctails");
+let coctail = document.querySelector("#coctail");
+let load = document.querySelector("#loading");
+
+
+// Enter button 
+inputFeild.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.key == "Enter") {
+        seacrhBtn.click();
+    }
+});
 
 
 
-        fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+    .then(res => res.json())
+    .then(data => displayData(data.drinks))
+
+seacrhBtn.addEventListener("click", () => {
+
+    if (isNaN(inputFeild.value) == false || inputFeild.value == "") {
+        alert("please enter valid input")
+    } else {
+
+        coctails.textContent = "";
+        load.style.display = "block";
+        console.log(inputFeild.value);
+        let url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputFeild.value}`
+        fetch(url)
             .then(res => res.json())
-            .then(data => displayData(data.drinks))
-
-        seacrhBtn.addEventListener("click", () => {
-
-            if (isNaN(inputFeild.value) == false || inputFeild.value == "") {
-                alert("please enter valid input")
-            } else {
-
-                coctails.textContent = "";
-                load.style.display = "block";
-                console.log(inputFeild.value);
-                let url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputFeild.value}`
-                fetch(url)
-                    .then(res => res.json())
-                    .then(data => {
+            .then(data => {
 
 
-                        setTimeout(() => {
-                            displayData(data.drinks);
+                setTimeout(() => {
+                    displayData(data.drinks);
 
-                        }, 500);
-                        setTimeout(() => {
+                }, 500);
+                setTimeout(() => {
 
-                            load.style.display = "none";
+                    load.style.display = "none";
 
-                        }, 500);
+                }, 500);
 
 
 
-                    })
+            })
 
 
-                inputFeild.value = "";
-            }
+        inputFeild.value = "";
+    }
 
-        })
+})
 
-        function displayData(datas) {
-            if (datas == null) {
-                alert("Data Not Avaiable")
+function displayData(datas) {
+    if (datas == null) {
+        alert("Data Not Avaiable")
 
-            } else {
+    } else {
 
-                datas.forEach(data => {
-                    console.log(data.strDrink);
-                    let div = document.createElement("div");
-                    div.innerHTML = `
+        datas.forEach(data => {
+            console.log(data.strDrink);
+            let div = document.createElement("div");
+            div.innerHTML = `
 
                             <img src="${data.strDrinkThumb}" alt="">
                             <h5>Drink Name : ${data.strDrink}</h5>
                             <p>Catagory  : ${data.strCategory}</p>
                 
                 `;
-                    coctails.appendChild(div);
+            coctails.appendChild(div);
 
-                    div.addEventListener("click", () => {
-                        coctail.textContent = "";
-                        let Div1 = document.createElement("div");
+            div.addEventListener("click", () => {
+                coctail.textContent = "";
+                location.hash = "#coctail";
+                let Div1 = document.createElement("div");
 
-                        console.log(data.strDrink);
+                console.log(data.strDrink);
 
-                        Div1.innerHTML = `
+                Div1.innerHTML = `
                                 
                                 <img src="${data.strDrinkThumb}" alt="">
                                 <h3>Drink Name : ${data.strDrink}</h3>
@@ -82,11 +92,11 @@
                                 <p>Instructions : ${data.strInstructions.slice(0, 150)}</p>
 
                     `;
-                        coctail.appendChild(Div1);
+                coctail.appendChild(Div1);
+                location.hash = "#";
 
+            })
+        });
+    }
 
-                    })
-                });
-            }
-
-        }
+}
